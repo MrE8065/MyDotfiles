0827d75a3761c6e030a86174323c53593509b42d
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2026-01-28 14:08:23 +0100

    Updated nvim lsp config and updated packages

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -22,8 +22,7 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
+      vim.lsp.config("lua_ls.setup",{
         capabilities = capabilities
       })
 
